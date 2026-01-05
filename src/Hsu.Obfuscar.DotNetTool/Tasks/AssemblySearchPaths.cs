a5889d32314ef5feb9c3e8b0ded1688cd33b3cf1
hsu <seayxu@gmail.com>
2026-01-05 13:47:48 +0800

    updated props

---
+++ src/Hsu.Obfuscar.DotNetTool/Tasks/AssemblySearchPaths.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+public sealed class GetAssemblySearchPaths : Task
+{
+    [Required] public string TargetAssembly { get; set; }
+    [Required] public string ReferencePaths { get; set; }
+    [Required] public string OutputFile { get; set; }
+
+    public override bool Execute()
+    {
+        Log.LogMessage(MessageImportance.Normal, "Generating Obfuscar `AssemblySearchPaths` ...");
+        var paths = ReferencePaths
+            .Split(';')
+            .ToDictionary(Path.GetFileNameWithoutExtension, Path.GetDirectoryName);
+
+        var assembly = Assembly.LoadFrom(TargetAssembly);
+        var assemblies = assembly.GetReferencedAssemblies();
+
+        var builder = new StringBuilder("<?xml version=\"1.0\"?>");
+        builder.AppendLine("  <Include>");
+        var counter = 0;
+        foreach(var item in assemblies)
+        {
+            try
+            {
+                var asm = Assembly.Load(item);
+            }
+            catch (FileNotFoundException)
+            {
+                if (!paths.TryGetValue(item.Name, out var path)) continue;
+                builder.AppendLine($"    <AssemblySearchPath path='{path}' />");
+                counter++;
+            }
+        }
+
+        builder.AppendLine("  </Include>");
+        File.WriteAllText(OutputFile, builder.ToString());
+
+        Log.LogMessage(MessageImportance.Normal, $"Generated {counter} Obfuscar `AssemblySearchPaths`");
+        return true;
+    }
+}
---
+++ src/Hsu.Obfuscar.DotNetTool/Tasks/ObfuscarFileGen.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+public sealed class ObfuscarFileGen : Task
+{
+    [Required] public string TargetFileName { get; set; }
+    [Required] public string ObfuscatedDir { get; set; }
+    [Required] public string ObfuscarAssembliesName { get; set; }
+    [Required] public string OutputFile { get; set; }
+
+    public override bool Execute()
+    {
+        Log.LogMessage(MessageImportance.Normal, "Generate Obfuscar config file skipped");
+        if (File.Exists(OutputFile)) return true;
+
+        Log.LogMessage(MessageImportance.Normal, "Generating Obfuscar config file...");
+        var template = $"""
+                        <?xml version="1.0"?>
+                        <Obfuscator>
+                          <!-- https://docs.obfuscar.com/getting-started/configuration -->
+                          <!-- https://github.com/obfuscar/obfuscar/blob/master/Obfuscar/Settings.cs -->
+                          
+                          <Var name="InPath" value="." />
+                          <Var name="OutPath" value=".\{ObfuscatedDir}" />
+                          <Var name="RenameFields" value="true" />
+                          <Var name="RenameProperties" value="true" />
+                          <Var name="RenameEvents" value="true" />
+                          <Var name="KeepPublicApi" value="true" />
+                          <Var name="HidePrivateApi" value="true" />
+                          <Var name="ReuseNames" value="true" />
+                          <Var name="UseUnicodeNames" value="false" />
+                          <Var name="UseKoreanNames" value="false" />
+                          <Var name="HideStrings" value="false" />
+                          <Var name="OptimizeMethods" value="true" />
+                          <Var name="SuppressIldasm" value="false" />
+                        
+                          <Module file="{TargetFileName}">
+                            <SkipEnums value="true" />
+                          </Module>
+                          
+                          <Include path="{ObfuscarAssembliesName}.xml" />
+                        </Obfuscator>
+                        """;
+        File.WriteAllText(OutputFile, template);
+        Log.LogMessage(MessageImportance.Normal, "Generated Obfuscar config file");
+
+        return true;
+    }
+}
